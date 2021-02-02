44cb7d536930dd25f332800e2efb99fe5151f0c6
Miu <miu@Mius-MacBook-Pro.local>
2021-02-02 11:46:31 -0800

    assignment

--- 00-dad-jokes/app.js
+++ 00-dad-jokes/app.js
@@ -3,28 +3,19 @@ const button = document.querySelector('button');
 const ul = document.querySelector('ul');
 
 const getDataJokes = async () => {
-    const res = await axios.get('https://icanhazdadjoke.com', {
+    return res = await axios.get('https://icanhazdadjoke.com', {
         headers: { 'Accept': 'application/json' }
     });
-    // console.log(res.joke);
-    addJokes(res.data.joke);
 };
 
-// getDataJokes();
-
-const addJokes = (joke) => {
-    const li = document.createElement('li');
-    li.innerHTML = joke;
-    ul.appendChild(li);
-}
-
-// function addJokes(joke) {
-//     const li = document.createElement('li');
-//     li.innerHTML = joke;
-//     ul.appendChild(li);
-// }
-
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    getDataJokes();
+  getDataJokes()
+  .then(() => {
+    const li = document.createElement('li');
+    li.innerHTML = res.data.joke;
+    ul.appendChild(li);
+  })
+  .catch(() => {
+    console.log('Error!');
+  })
 })
